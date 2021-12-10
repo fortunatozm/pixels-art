26d409f172887ebbe35a7f1fd4ee06fd38647aba
fortunatozm <fortunatompongo@hotmail.com>
2021-12-09 22:10:38 -0300

    Requisito 4 - OK

---
+++ script.js
@@ -0,0 +1,29 @@
+
+let pixelBoard = document.querySelector("#pixel-board");
+let line = 5;
+let colum = 5;
+
+for (let l = 0; l < line; l++) {
+  
+  for (let c = 0; c < colum; c++) {
+    
+  let divElement = document.createElement('div');
+  divElement.className = "pixel";
+
+  if ( divElement.style.backgroundColor == 'white' ){
+
+    pixelBoard.appendChild(divElement); 
+
+  } else{
+
+    divElement.style.backgroundColor = 'white';
+    pixelBoard.appendChild(divElement); 
+  }
+ 
+  }
+
+  let newLine = document.createElement ('br');
+  pixelBoard.appendChild(newLine);
+  
+}
+
